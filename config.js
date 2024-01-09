ef5f38cfc55b47443ecdc1fbee7eae131af4fb4f
Dominic-github <78993276+Dominic-github@users.noreply.github.com>
2024-01-09 10:43:01 +0700

    Init Project

---
+++ config.js
@@ -0,0 +1,33 @@
+
+var config = {
+   
+    texts: [
+        "Châu à!",    
+        "Chúc mừng sinh nhật bạn", 
+        "Tuổi mới hay ăn chóng lớn",
+        "Sức khoẻ dồi dào",
+        "Phát lộc phát tài",
+        "Vạn sự như ý",
+        "Có nhiều người yêu =)))",
+        "Thật nhiều hạnh phúc",
+        "Công việc suôn sẻ",
+        "Coder giàu có",
+        "và 🚭🚭🚭🚭🚭",
+    ],
+
+    imgs: {
+        "Châu": "./imgs/chau.jpg",
+  
+    },
+
+    desc: {
+        turn_on: "Bật đèn",
+        play: "Bật nhạc",
+        bannar_coming: "Hãy trang trí với tôi nào!!!",
+        balloons_flying: "Thêm bóng bay",
+        cake_fadein: "Thêm chiếc bánh ngon nhất từ trước đến nay",
+        light_candle: "Thêm nến nào",
+        wish_message: "Happy Birthday",
+        story: "Tôi có lời chúc đến bạn...",
+    }
+};
